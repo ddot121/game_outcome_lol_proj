7df190d9351bb79b42f4606976df3106eed6a271
Donnie Aikins <okafindo@protonmail.com>
2022-05-30 00:09:01 +0100

    changed players scrapped per elo to 40 (prev 100)

--- summoner_name_collection.py
+++ summoner_name_collection.py
@@ -11,23 +11,23 @@ def getAPI_key():
     return f.read()
 
 def getGoldPlayers(division):
-    """Retrieve 100 players in Gold x, where x is the specified division
+    """Retrieve 40 players in Gold x, where x is the specified division
     e.g. GOLD IV, GOLD III..."""
 
     players = []
     y = cass.get_paginated_league_entries(queue= cass.Queue.ranked_solo_fives, tier= "GOLD", division= division, region= "EUW")
-    for entry in y[:100]:
+    for entry in y[:40]:
         players.append(entry.summoner.sanitized_name)
             
     return players
 
 def getTierPlayers(tier, division):
-    """Retrieve 100 players in given tier x, where x is the specified division
+    """Retrieve 40 players in given tier x, where x is the specified division
     e.g. GOLD IV, SILVER II..."""
 
     players = []
     y = cass.get_paginated_league_entries(queue= cass.Queue.ranked_solo_fives, tier= tier, division= division, region= "EUW")   
-    for entry in y[:100]:
+    for entry in y[:40]:
         players.append(entry.summoner.sanitized_name)
             
     return players
